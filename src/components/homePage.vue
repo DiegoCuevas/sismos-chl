ceb2a090e3e14219b3087162db90288c09c11fc5
Diego Cuevas <diegocuevas579@gmail.com>
2020-02-25 20:27:09 -0500

    fetch and show earthquakes

---
+++ src/components/homePage.vue
@@ -0,0 +1,34 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar color="dark">
+        <ion-title>Sismos en chile</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content padding>
+      <ion-list v-for="sismo in sismos" :key="sismo.id">{{sismo.reference}}</ion-list>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      sismos: []
+    };
+  },
+
+  mounted() {
+    axios
+      .get(`https://chilealerta.com/api/query/?user=demo&select=ultimos_sismos`)
+      .then(response => response.data)
+      .then(data => {
+        this.sismos = data.ultimos_sismos;
+      });
+  }
+};
+</script>
+
+<style></style>
